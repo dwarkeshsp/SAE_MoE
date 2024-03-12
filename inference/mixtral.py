7bb80df9099c2983806027b0e137e799ca30cfd6
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2024-03-12 18:05:14 +0000

    want to try out mixtral inference basic file

---
+++ inference/mixtral.py
@@ -0,0 +1,66 @@
+import torch
+import pandas as pd
+from tqdm import tqdm
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from torch import nn
+import collections
+
+
+def load_dataset(filename, batch_size=32, dataset_relative_path="../dataset/"):
+    dataset = pd.read_csv(f"{dataset_relative_path}{filename}")
+    dataset_list = dataset["text"].tolist()
+    dataloader = torch.utils.data.DataLoader(dataset_list, batch_size=batch_size)
+    return dataloader
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
+
+double_quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    "mistralai/Mixtral-8x7B-v0.1",
+    quantization_config=double_quant_config,
+    # attn_implementation="flash_attention_2",
+)
+
+activation = collections.defaultdict(list)
+
+
+def getActivation(name):
+    def hook(model, input, output):
+        assert type(output) is not tuple
+        activation[name].append(output.detach())
+
+    return hook
+
+
+experts = model.model.layers[15].block_sparse_moe.experts
+
+for expert_idx in range(8):
+    experts[expert_idx].w3.register_forward_hook(
+        getActivation(f"layer_15_expert_{expert_idx}_w_3")
+    )
+    experts[expert_idx].act_fn.register_forward_hook(
+        getActivation(f"layer_15_expert_{expert_idx}_act_w1")
+    )
+    experts[expert_idx].w2.register_forward_hook(
+        getActivation(f"layer_15_expert_{expert_idx}_w_2")
+    )
+
+for batch in tqdm(load_dataset("wikitext_44836.csv")):
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
+    batch_tokens = tokenizer(
+        batch, padding=True, truncation=True, return_tensors="pt"
+    ).to(device)
+
+    generated_ids = model.generate(**batch_tokens, max_new_tokens=1, do_sample=False)
+    print(tokenizer.batch_decode(generated_ids)[0])
+
+    print(batch)
+    break
--- utils/datagen.py
+++ utils/datagen.py
@@ -7,22 +7,35 @@ import pandas as pd
 
 def generate_dataset(num_samples=110000, datasets=[], data_splits=[]):
     for i, dataset in enumerate(datasets):
-        data = load_dataset(dataset, "all")
-        print(len(data))
+        data = load_dataset(
+            dataset,
+            "wikitext-2-v1",
+            # split="train",
+        )
+        print(data)
         all = concatenate_datasets([dataset for dataset in data.values()])
         length = len(all)
-        data_samples = int(num_samples * data_splits[i])
+        data_samples = int(min(num_samples, length) * data_splits[i])
         print(length)
         print(data_samples)
         indices = random.sample(range(length), data_samples)
         sampled_data = all.select(indices)
         sampled_data = pd.DataFrame(sampled_data)
-        sampled_data.to_csv(f"../dataset/{dataset}_{data_samples}.csv", index=False)
+        sampled_data.replace("", pd.NA, inplace=True)
+        sampled_data.replace("\n", pd.NA, inplace=True)
+        sampled_data = sampled_data.dropna()
+        filename = dataset.split("/")[-1]
+        sampled_data.to_csv(
+            f"{dataset_relative_path}{filename}_{data_samples}.csv", index=False
+        )
         print(f"Generated {dataset}_{data_samples}.csv")
 
 
-generate_dataset(
-    num_samples=110000,
-    datasets=["cais/mmlu", "bigcode/the-stack-v2", "wikitext"],
-    data_splits=[0.10, 0.40, 0.5],
-)
+# generate_dataset(
+#     num_samples=1800000,
+#     # datasets=["cais/mmlu", "bigcode/the-stack-v2", "wikitext"],
+#     datasets=["wikitext"],
+#     data_splits=[1],
+# )
+
+load_dataset("wikitext_44836.csv")
