8054721f571dd2fd05b54c890d7058ed50a895cb
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2024-03-13 14:39:41 -0700

    Update load_instance.sh

--- scripts/load_instance.sh
+++ scripts/load_instance.sh
@@ -4,6 +4,9 @@ echo "Hello, world!"
 cd /root/
 ssh-keygen
 cat .ssh/id_rsa.pub
+huggingface-cli login --token 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
+git config --global user.name "Dwarkesh Patel"
+git config --global user.email "dwarkesh.sanjay.patel@gmail.com"
 git clone https://github.com/dwarkeshsp/SAE_MoE.git
 cd SAE_MoE
 pip install -r requirements.txt
