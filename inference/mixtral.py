67ab4e693205cc0be62eb00b3692380fe14430e1
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2024-03-13 17:27:08 +0000

    datagen done

--- inference/mixtral.py
+++ inference/mixtral.py
@@ -12,59 +12,23 @@ from transformers import (
 from torch import nn
 import collections
 import time
-
-
-start = time.time()
-print("started")
-
-
-def load_generated_dataset(
-    filename, batch_size=32, dataset_relative_path="../dataset/"
-):
-    dataset = pd.read_csv(f"{dataset_relative_path}{filename}")
-    # print(dataset.head(10))
-    dataset_list = dataset["text"].tolist()
-    dataloader = torch.utils.data.DataLoader(dataset_list, batch_size=batch_size)
-    return dataloader
-
-
-print(f"function defined: {time.time() - start}")
-
-# load_generated_dataset("wikitext_44836.csv")
+import os
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-print(f"device set, to load tok: {time.time() - start}")
-
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
-print(tokenizer.eos_token)
-
-print(f"tokenizer loaded: {time.time() - start}")
 
 double_quant_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
 )
 
-print(f"quantize config done: {time.time() - start}")
-
-# model = AutoModelForCausalLM.from_pretrained(
-#     "mistralai/Mixtral-8x7B-v0.1",
-#     quantization_config=double_quant_config,
-#     attn_implementation="flash_attention_2",
-# )
-
 layer = 15
-
 
 config = MixtralConfig(
     num_experts_per_tok=8,
     num_hidden_layers=layer,
 )
-
-print(f"config done: {time.time() - start}")
-
-# model = MixtralModel(config).from_pretrained("quantized")
 
 model = MixtralModel(config).from_pretrained(
     "mistralai/Mixtral-8x7B-v0.1",
@@ -72,17 +36,7 @@ model = MixtralModel(config).from_pretrained(
     attn_implementation="flash_attention_2",
 )
 
-print(f"end: {time.time() - start}")
-
 # %%
-
-output_dir = "quantized/"
-model.save_pretrained(output_dir)
-
-
-# %%
-
-
 activation = collections.defaultdict(list)
 
 
@@ -103,15 +57,28 @@ for expert_idx in range(8):
     experts[expert_idx].act_fn.register_forward_hook(
         getActivation(f"layer_{layer}_expert_{expert_idx}_act_w1")
     )
-    # experts[expert_idx].w2.register_forward_hook(
-    #     getActivation(f"layer_15_expert_{expert_idx}_w_2")
-    # )
+# %%
 
-dataset = load_generated_dataset("wikitext_44836.csv", batch_size=1)
+
+def load_generated_dataset(
+    filename, batch_size=32, dataset_relative_path="../dataset/"
+):
+    dataset = pd.read_csv(f"{dataset_relative_path}{filename}")
+    # print(dataset.head(10))
+    dataset_list = dataset["text"].tolist()
+    for i in range(len(dataset_list)):
+        print(i, dataset_list[i])
+    dataloader = torch.utils.data.DataLoader(dataset_list, batch_size=batch_size)
+    return dataloader
+
+
+dataset = load_generated_dataset("pile.csv", batch_size=2)
+
+# %%
 
 i = 0
 for batch in tqdm(dataset):
-    if i > 10:
+    if i > 3:
         break
     i += 1
 
--- utils/datagen.py
+++ utils/datagen.py
@@ -1,73 +1,75 @@
-# %%
-from datasets import load_dataset, concatenate_datasets
+from datasets import load_dataset
 
-import random
 import pandas as pd
+import torch
 
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1")
 
-# %%
+
+def clean_df(samples_df):
+    samples_df.replace("", pd.NA, inplace=True)
+    samples_df.replace("\n", pd.NA, inplace=True)
+    samples_df = samples_df.dropna()
+    return samples_df
 
 
-def generate_dataset(
-    num_samples=110000, datasets=[], data_splits=[], dataset_relative_path="../dataset/"
+def generate_dataset(num_samples, dataset_relative_path="../dataset/"):
+    data = load_dataset("monology/pile-uncopyrighted", split="train", streaming="true")
+    i = 0
+    samples = []
+    for item in iter(data):
+        sample = item["text"]
+        samples.append(sample)
+        i += 1
+        if i == num_samples:
+            break
+    samples_df = pd.DataFrame(samples)
+    print(samples_df.head(5))
+
+    samples_df = clean_df(samples_df)
+
+    def split_sequence(sequence, max_length):
+        tokens = tokenizer.encode(sequence)
+        sequences = []
+        start_idx = 0
+        while start_idx < len(tokens):
+            end_idx = min(start_idx + max_length, len(tokens))
+            sequences.append(tokenizer.decode(tokens[start_idx:end_idx]))
+            start_idx = end_idx
+        return sequences
+
+    max_length = 128  # Specify the desired maximum sequence length
+    samples_df = samples_df[0].apply(lambda x: split_sequence(x, max_length))
+    samples_df = samples_df.explode("text")
+
+    samples_df = clean_df(samples_df)
+
+    print(samples_df.head(5))
+
+    samples_df.to_csv(f"{dataset_relative_path}/pile.csv", index=False)
+
+
+def load_generated_dataset(
+    filename, batch_size=32, dataset_relative_path="../dataset/"
 ):
-    for i, dataset in enumerate(datasets):
-        data = load_dataset(
-            dataset,
-            "wikitext-2-v1",
-            # split="train",
-        )
-        print(data)
-        all = concatenate_datasets([dataset for dataset in data.values()])
-        length = len(all)
-        data_samples = int(min(num_samples, length) * data_splits[i])
-        print(length)
-        print(data_samples)
-        indices = random.sample(range(length), data_samples)
-        sampled_data = all.select(indices)
-        sampled_data = pd.DataFrame(sampled_data)
-
-        sampled_data.replace("", pd.NA, inplace=True)
-        sampled_data.replace("\n", pd.NA, inplace=True)
-        sampled_data = sampled_data.dropna()
-
-        def split_sequence(sequence, max_length):
-            tokens = tokenizer.encode(sequence)
-            sequences = []
-            start_idx = 0
-            while start_idx < len(tokens):
-                end_idx = min(start_idx + max_length, len(tokens))
-                sequences.append(tokenizer.decode(tokens[start_idx:end_idx]))
-                start_idx = end_idx
-            return sequences
-
-        # Split the text into multiple sequences of the desired length
-        max_length = 128  # Specify the desired maximum sequence length
-        sampled_data["text"] = sampled_data["text"].apply(
-            lambda x: split_sequence(x, max_length)
-        )
-        sampled_data = sampled_data.explode("text")
-
-        sampled_data.replace("", pd.NA, inplace=True)
-        sampled_data.replace("\n", pd.NA, inplace=True)
-        sampled_data = sampled_data.dropna()
-
-        filename = dataset.split("/")[-1]
-        sampled_data.to_csv(
-            f"{dataset_relative_path}{filename}_{data_samples}.csv", index=False
-        )
-        print(f"Generated {dataset}_{data_samples}.csv")
+    dataset = pd.read_csv(f"{dataset_relative_path}/{filename}")
+    print(dataset.head(10))
+    dataset_list = dataset["0"].tolist()
+    for i in range(len(dataset_list)):
+        print(i, dataset_list[i])
+    dataloader = torch.utils.data.DataLoader(dataset_list, batch_size=batch_size)
+    return dataloader
 
 
-generate_dataset(
-    num_samples=1800000,
-    # datasets=["cais/mmlu", "bigcode/the-stack-v2", "wikitext"],
-    datasets=["wikitext"],
-    data_splits=[1],
-)
+dataset = load_generated_dataset("pile.csv", batch_size=2)
 
+# generate_dataset(
+#     num_samples=1000,
+#     # datasets=["cais/mmlu", "bigcode/the-stack-v2", "wikitext"],
+#     # datasets=["wikitext"],
+#     # data_splits=[1],
+# )
 
 # load_dataset("wikitext_44836.csv")
