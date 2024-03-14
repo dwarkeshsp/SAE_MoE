b748009792e4c01cd80443b8327ef814c1c7f291
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2024-03-13 19:18:58 -0700

    Update load_instance.sh

--- scripts/load_instance.sh
+++ scripts/load_instance.sh
@@ -2,7 +2,6 @@
 
 echo "Hello, world!"
 cd /root/
-ssh-keygen
 huggingface-cli login --token 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
 git config --global user.name "Dwarkesh Patel"
 git config --global user.email "dwarkesh.sanjay.patel@gmail.com"
