2e69d1053aecc2d1b2bd203523428c307137b667
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2024-03-18 09:17:23 -0700

    Update load_instance.sh

--- scripts/load_instance.sh
+++ scripts/load_instance.sh
@@ -8,6 +8,3 @@ git config --global user.email "dwarkesh.sanjay.patel@gmail.com"
 git clone https://github.com/dwarkeshsp/SAE_MoE.git
 cd SAE_MoE
 pip install -r requirements.txt
-cd utils
-python load_model.py
-
